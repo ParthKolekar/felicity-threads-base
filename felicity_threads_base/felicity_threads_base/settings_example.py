6fbedfc2f442e5bc07c42ce3b5fb4e2dbbccca7d
Abhineet Jain <abhiineitjain@gmail.com>
2015-01-07 11:59:01 +0530

    settings updated, create empty media, static folder in the django root

--- felicity_threads_base/felicity_threads_base/settings_example.py
+++ felicity_threads_base/felicity_threads_base/settings_example.py
@@ -90,5 +90,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = (TEMPLATE_PATH,)
