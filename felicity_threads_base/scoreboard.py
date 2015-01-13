c460351efdb41580912e9086bfe5dc9b79ea891e
root <root@selina>
2015-01-13 15:34:49 +0000

    Added py

---
+++ felicity_threads_base/scoreboard.py
@@ -0,0 +1,8 @@
+import _mysql
+import json
+db = _mysql.connect("localhost" , "root" , "saphira" , "felicity_threads_base")
+db.query("""SELECT user_nick , user_score FROM base_user ORDER BY user_score DESC , user_total_time ASC""")
+r = db.store_result()
+f = open('score.json' , 'w')
+json.dump(r.fetch_row() , f)
+
