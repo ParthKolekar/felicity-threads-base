8ebf77ebdbc603fd7019df361fb300e93469a0b7
root <root@selina>
2015-01-13 17:58:18 +0000

    New db config

--- felicity_threads_base/scoreboard.py
+++ felicity_threads_base/scoreboard.py
@@ -1,8 +1,8 @@
 import _mysql
 import json
-db = _mysql.connect("localhost" , "root" , "saphira" , "felicity_threads_base")
+db = _mysql.connect("192.168.1.1" , "contest" , "saphira" , "felicity_threads_base")
 db.query("""SELECT user_nick , user_score FROM base_user ORDER BY user_score DESC , user_total_time ASC""")
 r = db.store_result()
 f = open('static/score.json' , 'w')
-json.dump(r.fetch_row() , f)
+json.dump( r.fetch_row( r.num_rows() ), f )
 
