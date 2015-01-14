0a4c8b378c15be93f0e2a8c868d07cf064305ab3
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-01-14 15:44:03 +0000

    changed host from 192.168.1.1 to 10.4.8.62
    For no fucking reason :P (ASAIK)

--- felicity_threads_base/scoreboard.py
+++ felicity_threads_base/scoreboard.py
@@ -1,6 +1,6 @@
 import _mysql
 import json
-db = _mysql.connect("192.168.1.1" , "contest" , "saphira" , "felicity_threads_base")
+db = _mysql.connect("10.4.8.62" , "contest" , "saphira" , "felicity_threads_base")
 db.query("""SELECT user_nick , user_score, submission_timestamp FROM base_user JOIN cache_in_submission JOIN cache_in_question WHERE submission_state = 'AC' AND base_user.id = submission_user_id AND submission_question_id = cache_in_question.id AND user_access_level = question_level + 1 GROUP BY user_nick ORDER BY user_score DESC, submission_timestamp ASC;
 """)
 r = db.store_result()
