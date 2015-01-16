e6f9ef02a2abb02c5e48f2ea1f6425ab09722573
root <root@selina>
2015-01-16 15:02:55 +0000

    Test Blocker again

--- felicity_threads_base/base/middleware.py
+++ felicity_threads_base/base/middleware.py
@@ -18,15 +18,15 @@ class RestrictAccessTillTime(object):
         pass
 
     def process_view(self, request, view_func, view_args, view_kwargs):
+	return None
         if request.user.is_authenticated():
+            if request.user.is_staff:
+            	return None
             time_now = datetime.datetime.now(utc)
-            if time_now > settings.CONTEST_START_DATETIME and time_now < settings.CONTEST_END_DATETIME:
-                return None
+            if time_now < settings.CONTEST_START_DATETIME or time_now > settings.CONTEST_END_DATETIME:
+            	error = ('<h1>Forbidden</h1><p>Contest Not Started</p>')
+                return HttpResponseForbidden(error)
             else:
-                if request.user.is_staff:
-                    return None
-                else:
-                    error = ('<h1>Forbidden</h1><p>Contest Not Started</p>')
-                    return HttpResponseForbidden(error)
+		return None
         else:
             return None
