851dbd09db8cbcf1d3705896d492bc031452618a
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-02-08 00:05:48 +0530

    forgot to add folders

---
+++ felicity_threads_base/blast_off/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, url
+
+from django.conf import settings
+from django.conf.urls.static import static
+from blast_off import views
+
+urlpatterns = patterns('',
+	url(r'^$' , views.index, name='index'),
+    url(r'^problems$', views.problems, name='problems'),
+    #url(r'^submissions$', views.submissions, name='submissions'),
+
+    #regex \d{1,2} means that the question level or question id will be at max 99.
+    url(r'^question/(\d{1,2})/(\d{1,2})$', views.question, name='question'),
+    url(r'^submit/(\d{1,2})/(\d{1,2})$', views.submit, name='submit'),
+    url(r'^comment_submit/(\d{1,2})/(\d{1,2})$', views.comment_submit, name='comment_submit'),
+    url(r'^scoreboard$', views.scoreboard, name='scoreboard'),
+)
