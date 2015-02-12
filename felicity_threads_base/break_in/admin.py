8339c6e10a7324d4a4c98743354b87b04d5b54e5
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-02-12 23:14:36 +0530

    Added break_in

---
+++ felicity_threads_base/break_in/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from break_in.models import *
+
+# Register your models here.
+admin.site.register(Question)
+admin.site.register(Submission)
+admin.site.register(Comment)
