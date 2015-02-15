750df33a97ddef09170d688108fedc0e630aef21
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-02-15 12:06:59 +0530

    Code Da Circuit

---
+++ felicity_threads_base/code_da_circuit/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from code_da_circuit.models import *
+
+# Register your models here.
+admin.site.register(Question)
+admin.site.register(Submission)
+admin.site.register(Comment)
